b0e782267e293615791a1c2a6d0a3b5a21ea6579
lilo <williamcco@gmail.com>
2019-10-27 00:01:29 -0300

    layout stylization, signin and signup pages, redux store, actions and reducer, saga, api service and reactotron settings

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Enter a valid e-mail address.')
+    .required('E-mail is required.'),
+  password: Yup.string().required('Password is required.'),
+});
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
+
+        <button type="submit">Sign in</button>
+        <Link to="/register">Register for free</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required.'),
+  email: Yup.string()
+    .email('Enter a valid e-mail address.')
+    .required('E-mail is required.'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters.')
+    .required('Password is required.'),
+});
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
+
+        <button type="submit">Create account</button>
+        <Link to="/">Already a member</Link>
+      </Form>
+    </>
+  );
 }
