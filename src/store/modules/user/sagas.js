0ccab7a09c7396171aa7c1eb24b6563ce62fc611
lilo <williamcco@gmail.com>
2019-10-29 00:02:10 -0300

    updating profile

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+import api from '~/services/api';
+
+export function* updateProfile({ payload }) {
+  const { name, email, ...rest } = payload.data;
+
+  const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+  try {
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile successfully updated.');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error('Error updating profile, check your data.');
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
