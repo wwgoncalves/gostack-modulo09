21963c09ab12b768ae6929380c2776665785e457
lilo <williamcco@gmail.com>
2019-10-28 23:38:20 -0300

    styling profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail address" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Your new password"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm your new password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Sign out of GoBarber</button>
+    </Container>
+  );
 }
